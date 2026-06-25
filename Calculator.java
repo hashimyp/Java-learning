a5afe28029d7820e9b3454d7b5fa0af4bc92820a
hashimyp <hashimyp89@gmail.com>
2026-06-25 17:31:54 +0530

    calculator usinf if else and switch

---
+++ Calculator.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+public class Calculator {
+public static void main(String[] args) {
+    System.out.println("Fast Calculator");
+    Scanner sc = new Scanner(System.in);
+    int i = sc.nextInt();
+int j = sc.nextInt();
+
+    System.out.println("Enter the operation you want to perform: +, -, *, /");
+    char op = sc.next().charAt(0);
+    int result;
+    switch (op) {
+        case '+':
+            result = i + j;
+            System.out.println(i + " + " + j + " = " + result);
+            break;
+        case '-':
+            result = i - j;
+            System.out.println(i + " - " + j + " = " + result);
+            break;
+        case '*':
+            result = i * j;
+            System.out.println(i + " * " + j + " = " + result);
+            break;
+        case '/':
+            if (j != 0) {
+                result = i / j;
+                System.out.println(i + " / " + j + " = " + result);
+            } else {
+                System.out.println("Error: Division by zero is not allowed.");
+            }
+            break;
+            case '%':
+            if (j != 0) {
+                result = i % j;
+                System.out.println(i + " % " + j + " = " + result);
+            } else {
+                System.out.println("Error: Division by zero is not allowed.");
+            }   
+        default:
+            System.out.println("Invalid operation!");
+            break;
+    }
+
+}
+}
