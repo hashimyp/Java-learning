50331e1c1b2bfdf1c0476c6996fffcf42fe9dbed
hashimyp <hashimyp89@gmail.com>
2026-07-06 17:00:54 +0530

    demo of basic functions in java

---
+++ Functions.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; // import the Scanner class to read input
+public class Functions {
+ public static void f1(String name) // argument is the input function require along with the datatype it can also be multiple inputs
+//function come before the main function and can be called in the main function  
+         {
+            System.out.println("Hello, " + name + "!");
+            return; // return statement is used to exit the function 
+         }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // create a Scanner object to read input
+        System.out.print("Enter your name: "); // prompt the user to enter their name   
+        String name = sc.nextLine(); // read the user's input
+        f1(name); // call the function f1 with the user's name as an argument
+        
+    }
+    
+}
---
+++ Multiplication_Table.java
@@ -0,0 +1,16 @@
+
+      
+import java.util.Scanner;
+
+public class Multiplication_Table {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: Make a TABLE of that number!");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        for (int j = 1; j <= 10; j++) {
+            int k = i * j;
+            System.out.println(i + " * " + j + " = " + k);
+        }
+        sc.close();
+    }
+}
