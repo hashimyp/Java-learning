ef7faf7d95b6071e09c4b70a048937d8c57caa68
hashimyp <hashimyp89@gmail.com>
2026-07-06 19:05:09 +0530

    multiplication table using for loop

--- Multiplication_Table.java
+++ Multiplication_Table.java
@@ -10,7 +10,7 @@ public class Multiplication_Table {
         for (int j = 1; j <= 10; j++) {
             int k = i * j;
             System.out.println(i + " * " + j + " = " + k);
-        }
+        }   
         sc.close();
     }
 }
