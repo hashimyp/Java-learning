fbf5bf131a1b496dddf913402ca954c82de7084f
hashimyp <hashimyp89@gmail.com>
2026-07-11 14:00:21 +0530

    to display power of a number using Math.pow() method in Java.

---
+++ fundamentals/Num_Power.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Num_Power {
+
+
+
+public static void main(String[] args) {
+    if () {
+        
+    }
+   Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the base number(x): ");
+    int x = sc.nextInt();
+    System.out.print("Enter the exponent(n): ");
+    int n = sc.nextInt();
+    int power = (int) Math.pow(x, n); // Calculate x raised to the power of n
+    System.out.println(x + " raised to the power of " + n + " is: " + power);
+    sc.close();
+
+
+
+
+
+}
+} 
