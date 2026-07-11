6616b977ccfd09f9d284b52db6d3f66b7907dcde
hashimyp <hashimyp89@gmail.com>
2026-07-11 13:31:05 +0530

    calculate circumderence of a circle

---
+++ fundamentals/circle_circumference.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class circle_circumference {
+
+    public static float circ(float radius) {
+        if (radius < 0) {
+            System.out.println("Please enter a positive integer.");
+            return (float) 0.0; // or 0.0f
+        }
+        final float PI = 3.14f; // constant value of pi
+        float circumference = 2 * PI * radius; // using the constant
+        return circumference;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter the radius of the circle:");
+        Scanner sc = new Scanner(System.in);
+        float radius = sc.nextFloat();
+        float circumference = circ(radius); // calling the function
+        System.out.println("Circumference of the circle is: " + circumference);
+
+        sc.close();
+    }
+}
