f8dc1f149faafb1b2893865d1fd2bdcd716393ae
hashimyp <hashimyp89@gmail.com>
2026-07-14 15:34:33 +0530

    Power of a number in java

--- fundamentals/Num_Power.java
+++ fundamentals/Num_Power.java
@@ -4,14 +4,12 @@ public class Num_Power {
 
 
 public static void main(String[] args) {
-    if () {
-        
+    if (args.length != 2) {
+        System.out.println("Please provide exactly two arguments: base and exponent.");
+        return;
     }
-   Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the base number(x): ");
-    int x = sc.nextInt();
-    System.out.print("Enter the exponent(n): ");
-    int n = sc.nextInt();
+    int x = Integer.parseInt(args[0]);
+    int n = Integer.parseInt(args[1]);
     int power = (int) Math.pow(x, n); // Calculate x raised to the power of n
     System.out.println(x + " raised to the power of " + n + " is: " + power);
     sc.close();
