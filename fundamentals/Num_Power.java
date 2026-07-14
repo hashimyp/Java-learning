51ab1b7580dec8a4191724f23dbb146de74b5c23
hashimyp <hashimyp89@gmail.com>
2026-07-14 22:00:03 +0530

    Basic program to calculate power of a number

--- fundamentals/Num_Power.java
+++ fundamentals/Num_Power.java
@@ -1,22 +1,19 @@
-import java.util.Scanner;
+
 public class Num_Power {
 
 
 
 public static void main(String[] args) {
-    if (args.length != 2) {
-        System.out.println("Please provide exactly two arguments: base and exponent.");
-        return;
-    }
+   
+
     int x = Integer.parseInt(args[0]);
     int n = Integer.parseInt(args[1]);
     int power = (int) Math.pow(x, n); // Calculate x raised to the power of n
     System.out.println(x + " raised to the power of " + n + " is: " + power);
-    sc.close();
-
-
+    
+ 
 
 
 
 }
-} 
+}
